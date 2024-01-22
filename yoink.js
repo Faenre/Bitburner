c88f185b2a554cacc38b9adf1fba46322f68f8ea
Nicole B <nicole@faenre.dev>
2024-01-22 01:44:44 -0600

    added full calibration

--- yoink.js
+++ yoink.js
@@ -9,42 +9,95 @@
 let NS;
 let Host;
 
-const GROW_SEC = 0.004
-const HACK_SEC = 0.002
-const WEAK_SEC = 0.050
+const GROW_SEC = 0.004;
+const HACK_SEC = 0.002;
+const WEAK_SEC = 0.050;
+
+let SecurityBuildup = 0.000;
 
 /** 
+ * This single-node script aims for optimum money farming.
+ * 
+ * At the start, it grows up to the server maximum, then it calculates
+ * how many growths are necessary per batch of hacks. It then uses this
+ * to minimize the number of growths and weakens, and maximize the 
+ * number of hacks.
+ * 
+ * The hostname file must either be set, or set to an argument, or it won't run.
+ * 
  * @param {NS} ns
  * */
 export async function main(ns) {
   NS = ns;
-  Host = NS.read('hostname.txt');
+  Host = NS.args[0] || NS.read('hostname.txt');
+
+  let iterations = await calibrate();
   
   while (true) {
-    await growToMax();
     await harvestCycle();
+    await regrow(iterations);
   }
 }
 
+/**
+ * Calibrates to figure out how many regrowths are necessary per batch of ~23 hacks.
+ * This can take a long time the first time it executes, but saves several minutes
+ * per batch, which adds up over time.
+ * */
+async function calibrate() {
+	// Set money to max (this might take a long time, the very first time executed)
+  while (await grow() > 1.000) {}
 
-async function growToMax(iter=0, sec=0.000) {
-  // NS.print('Growth iteration: ' + iter);
-  // NS.print('Security offset: ' + sec);
+  // Reset security to 0
+  while (SecurityBuildup >= 0.00) 
+  	await weaken();
 
-  if (sec >= WEAK_SEC){
-    await NS.weaken(Host);
-    sec -= WEAK_SEC;
-  }
+  // Run a maximum-cycle of hacks
+  const hacks = (WEAK_SEC - GROW_SEC) / HACK_SEC;
+  for (let i=0; i < hacks; i++) 
+  	await grow();
+  await weaken();
 
-  if (await NS.grow(Host) > 1.000) 
-    return growToMax(iter + 1, sec + GROW_SEC);
+  // Count how many iterations of regrowth are necessary
+  let regrowthIterations = 0;
+  while (await grow() > 1.000)
+  	regrowthIterations += 1;
+
+  // Calibration complete!
+  return regrowthIterations;
 }
 
 
-async function harvestCycle(sec=0.000) {
-  if (sec >= WEAK_SEC) 
-    return await NS.weaken(Host);
-  
-  await NS.hack(Host);
-  return await harvestCycle(sec + HACK_SEC);
+async function harvestCycle(sum) {
+  if (SecurityBuildup >= WEAK_SEC) {
+    await weaken();
+    return sum;
+  }
+  const amtStolen = await hack();
+  return await harvestCycle(sum + amtStolen);
+}
+
+
+async function regrow(counter) {
+	if (counter <= 0) return;
+	await grow();
+	return await regrow(counter - 1);
+}
+
+
+// NS function wrappers:
+
+async function grow() {
+	const growth = await NS.grow(Host);
+	if (growth > 1.000) 
+		SecurityBuildup += GROW_SEC;
+	return growth;
+}
+async function hack() {
+	SecurityBuildup += HACK_SEC;
+	return await NS.hack(Host);
+}
+async function weaken() {
+	SecurityBuildup -= WEAK_SEC;
+	return await NS.weaken(Host);
 }
