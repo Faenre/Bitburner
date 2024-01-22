680ffe028cde65f585c7c82f8ef2c88ac6cb6dff
Nicole B <nicole@faenre.dev>
2024-01-22 04:04:21 -0600

    removed call to gethostname

--- grow.js
+++ grow.js
@@ -3,7 +3,7 @@
  * @param {NS} ns 
  * */
 export async function main(ns) {
-	const target = ns.args[0] || ns.getHostname();
+	const target = ns.args[0];
 
   while (true) 
   	await ns.grow(target);
