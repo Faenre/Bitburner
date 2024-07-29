f453868bf2abaa0cd3d16c70def18cb8c955847a
Nicole B <nicole@faenre.dev>
2024-07-28 21:57:26 -0500

    added variable commands

--- src/threadpool.js
+++ src/threadpool.js
@@ -7,22 +7,33 @@
  * @arg {...} any options to provide each script
  * */
 export async function main(ns) {
-  ns.disableLog('getServerMaxRam');
-  ns.disableLog('getServerUsedRam');
-  ns.disableLog('scp');
-  ns.disableLog('exec');
-  ns.disableLog('killall');
+  ns.disableLog('ALL');
   ns.tail();
-  const script = ns.args[0];
-  const threads = ns.args[1];
-  const opts = ns.args.slice(2);
+  const command = ns.args[0];
+  ({
+    'doScript': doScript,
+    'info': info,
+    'killall': killall,
+  })[command](ns);
+}
 
-  const pool = RamPool.fromPServs(ns);
-  ns.print(`INFO Pool initiated`);
+function doScript(ns, pool) {
+  const script = ns.args[1];
+  const threads = ns.args[2];
+  const opts = ns.args.slice(3);
+
+  pool.assignThreads(script, threads, opts);
+  ns.print(`SUCCESS all threads assigned`);
+}
+
+function info(ns, pool) {
   ns.print(`INFO Pool has ${pool.servers.length} servers`);
   ns.print(`INFO Pool has ${pool.totalRamFree()} ram free`);
-  // pool.assignMaxThreads(script, opts);
-  pool.assignThreads(script, threads, opts);
+}
+
+function killall(ns, pool) {
+  pool.killAllRunningScripts();
+  ns.print(`SUCCESS all tasks terminated.`);
 }
 
 class RamPool {
@@ -46,11 +57,11 @@ class RamPool {
   }
 
   assignThreads(script, threads, opts) {
-    if (!threads) 
+    if (!threads)
       return this.assignMaxThreads(script, opts);
     let threadsRemaining = threads;
     for (let server of this.servers.sort(this.sortByRamFree)) {
-      if (!threadsRemaining) 
+      if (!threadsRemaining)
         return true;
       const threadsToUse = Math.min(threadsRemaining, server.maxThreads(script));
       server.runScript(script, threadsToUse, opts);
