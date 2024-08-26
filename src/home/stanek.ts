31e1585c35c672cbfbe9f72e062bdb8b5bde5b3c
Nicole B <nicole@faenre.dev>
2024-08-26 18:09:42 -0500

    depr the dist folder

--- src/home/stanek.ts
+++ src/home/stanek.ts
@@ -1,5 +1,5 @@
 import { ActiveFragment } from '@/NetscriptDefinitions';
-import { getFromApi } from '../HTTP/API';
+import { getFromApi } from './HTTP/API';
 
 /**
  * Utility to roate through stanek fragments.
@@ -11,18 +11,18 @@ import { getFromApi } from '../HTTP/API';
  */
 export async function main(ns: NS) {
 	ns.disableLog('stanek.chargeFragment');
-	ns.tail();
 
 	while (true) {
 		const fragments = await getFragmentCoords(ns);
 		for (let i=0; i < 100; i++)
 			for (const f of fragments)
 				await chargeFragment(ns, f);
+		await ns.sleep(1000);
 	}
 }
 
 async function getFragmentCoords(ns: NS): Promise<ActiveFragment[]> {
-	const request = {endpoint: 'stanek', callback: 2013, data: {} }
+	const request = {endpoint: 'stanek', callback: 2130, data: {} }
 	const fragments = (await getFromApi(ns, request)).data['fragments'];
 	return fragments;
 }
